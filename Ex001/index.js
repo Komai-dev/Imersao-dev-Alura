78da20f5d0de76c89dcc29fe3dfe894d92c2d433
Maiko Costa <122563962+Komai-dev@users.noreply.github.com>
2023-02-01 14:32:34 -0300

    a

--- Ex001/index.js
+++ Ex001/index.js
@@ -1,1 +1,6 @@
-alert('Estamos começando a Imersão!')
+var valorEmDolar = 30;
+var cotacaoDoDolar = 5.08;
+
+var valorEmReal = valorEmDolar * cotacaoDoDolar;
+var valorEmReal = valorEmReal.toFixed(2)
+alert('R$ ' + valorEmReal);
